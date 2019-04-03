f81c36301a1daa2c4565f1f7ff07775ead63a821
Chiien <fuchienhsu12@gmail.com>
2019-04-03 20:08:11 -0300

    Feat() add simple projet setup

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv'
+
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import routes from './routes'
+dotenv.config()
+
+class App {
+  public express: express.Application
+
+  public constructor () {
+    this.express = express()
+
+    this.middlewares()
+    this.database()
+    this.routes()
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private database (): void {
+    mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true
+    })
+  }
+
+  private routes (): void {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
